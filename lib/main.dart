edb472e5cc12237406c06a1d8271244418493329
nguyendang1308 <nguyenjamie1308@gmail.com>
2023-03-05 11:25:35 +0700

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PersonalFulState extends StatefulWidget {
+  const PersonalFulState({super.key});
+
+  @override
+  State<PersonalFulState> createState() => _PersonalApp();
+}
+
+class _PersonalApp extends State<PersonalFulState> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
